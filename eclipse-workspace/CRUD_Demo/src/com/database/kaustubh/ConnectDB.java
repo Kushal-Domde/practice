5b92a77bca6d380d593cfb100ad52373e09809c0
kaustubh <kushaldom61@gmail.com>
2022-07-08 21:17:48 +0530

    code of database

---
+++ eclipse-workspace/CRUD_Demo/src/com/database/kaustubh/ConnectDB.java
@@ -0,0 +1,27 @@
+package com.database.kaustubh;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConnectDB 
+{
+		public static void main(String args[]) 
+		{
+			    Connection c = null;
+			    try 
+			    {
+					if (c== null)
+					{
+						System.out.println("in Connection try .........");
+						Class.forName("org.postgresql.Driver");
+						c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres","postgres");
+						System.out.println("connection " + c);
+						System.out.println("Connection to database sucessfully");
+					}
+				}
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+					System.err.println(e.getClass().getName()+": "+e.getMessage());
+			         System.exit(0);
+				}
+		}
+}
